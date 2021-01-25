ad45b2e82fd76f24eb4ef2d960db551108f8fb46
Geraldo <geraldofjunior@gmail.com>
2021-01-25 18:50:58 -0300

    Making the initial page

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
-include 'ui/model/index.php';
+namespace Point_Calc_Php;
+use Point_Calc_Php\Services\SimpleRouter as SimpleRouter;
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="ui\style\home.css" rel="stylesheet" />
+    <link href="ui\style\button.css" rel="stylesheet" />
+    <link href="ui\style\card.css" rel="stylesheet" /> 
+    <script src="https://kit.fontawesome.com/6e2301b883.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="main">
+    <?php 
+    
+    include "ui/model/sidebar.php";
+    include "services/simple_router.php";
+    
+    $router = SimpleRouter::getInstance();
+    if (isset($_GET['P'])) {
+        $router->route($_GET['P']);
+    } else {
+        $router->route("ui\model\main.php");
+    }
+
+    ?>
+</div>
+     
+</body>
+</html>
---
+++ ui/model/sidebar.php
@@ -0,0 +1,3 @@
+<aside class="sidebar">
+    &nbsp;
+</aside>
