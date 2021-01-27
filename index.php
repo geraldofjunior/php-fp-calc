85d0a66393b3b100f977e5382b50aa2f80e72d17
Geraldo <geraldofjunior@gmail.com>
2021-01-27 14:27:56 -0300

    Adding the project detail page and a really simple router into index

--- index.php
+++ index.php
@@ -4,7 +4,19 @@ namespace Point_Calc_Php;
 include "ui/model/components/head.html";
 
 include "ui/model/components/sidebar.php";
-include "ui/model/pages/main.php";
+$addr = "ui/model/pages/";
+if (isset($_GET['p'])) {
+    if (!is_nan($_GET['p'])) {
+        $addr .= "project.php";
+    } else {
+        $addr .= "main.php";
+    }
+} else {
+    $addr .= "main.php";
+}
+
+include $addr;
+
 include "ui/model/components/buttons.html";
 
 include "ui/model/components/footer.html";
---
+++ ui/model/pages/project.php
@@ -0,0 +1,7 @@
+<main class="content">
+    <div class="overview">
+    </div>
+
+    <div class="function-list">
+    </div>
+</main>
