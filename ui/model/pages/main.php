bbeee4b8470aadd8f782977677b395cd9718a041
Geraldo <geraldofjunior@gmail.com>
2021-02-01 18:11:14 -0300

    Including the styles for the project page

--- ui/model/pages/main.php
+++ ui/model/pages/main.php
@@ -1,86 +1,66 @@
 <main class="content">
     
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Blego blego</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Pôjeto do Pofexô Luxa</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Love Calculator Capsule Corp.</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Site de Portfólio</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Bombril</div>
         <div class="card__info">1001 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Hello world</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">CRUD Básico</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">CRUD Gourmet</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Projeto 1</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Novo Projeto</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
     </div>
 
-
-
-    <div class="card">
+    <div class="card card--project">
         <div class="card__title">Lorem Ipsum</div>
         <div class="card__info">96 Funções</div>
         <div class="card__function-points">171 PF</div>
--- ui/model/pages/project.php
+++ ui/model/pages/project.php
@@ -1,9 +1,76 @@
+<link href="ui\style\info.css" rel="stylesheet" />
 <main class="content">
     <div class="overview">
-        <div class="info">Pontos contados</div>
-        <div class="info">Preço estimado</div>
-        <div class="info">Prazo estimado</div>
+        
+        <div class="card card--info">
+            <div class="card__title card--center">Pontos contados</div>
+            <div class="card__function-points card--center">1.715 PF</div>
+        </div>
+
+        <div class="card card--info">
+            <div class="card__title card--center">Preço estimado</div>
+            <div class="card__function-points card--center">R$ 21.725,10</div>
+        </div>
+
+        <div class="card card--info">
+            <div class="card__title card--center">Prazo estimado</div>
+            <div class="card__function-points card--center">36 dias</div>
+        </div>
+
     </div>
-    <div class="function-list">
-    </div>
+
+    <table class="function-list">
+
+        <thead class="function-list--header">
+
+            
+                <td class="function-list__name">Nome</td>
+                <td class="function-list__type">Tipo</td>
+                <td class="function-list__complexity">Complexidade</td>
+                <td class="function-list__contribution"><center>PF</center></td>
+            
+
+        </thead>
+
+        <tbody>
+
+            <tr>
+                <td class="function-list__name">Nome</td>
+                <td class="function-list__type">Tipo</td>
+                <td class="function-list__complexity">Complexidade</td>
+                <td class="function-list__contribution">PF</td>
+            </tr>
+
+            <tr>
+                <td class="function-list__name">Nome</td>
+                <td class="function-list__type">Tipo</td>
+                <td class="function-list__complexity">Complexidade</td>
+                <td class="function-list__contribution">PF</td>
+            </tr>
+
+            <tr>
+                <td class="function-list__name">Nome</td>
+                <td class="function-list__type">Tipo</td>
+                <td class="function-list__complexity">Complexidade</td>
+                <td class="function-list__contribution">PF</td>
+            </tr>
+
+            <tr>
+                <td class="function-list__name">Nome</td>
+                <td class="function-list__type">Tipo</td>
+                <td class="function-list__complexity">Complexidade</td>
+                <td class="function-list__contribution">PF</td>
+            </tr>
+
+            <tr>
+                <td class="function-list__name">Nome</td>
+                <td class="function-list__type">Tipo</td>
+                <td class="function-list__complexity">Complexidade</td>
+                <td class="function-list__contribution">PF</td>
+            </tr>
+
+        </tbody>
+
+    </table>
+    
 </main>
